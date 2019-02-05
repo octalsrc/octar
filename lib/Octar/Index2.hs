e39ffa69bc61fd28b36d03cbebacfc20fcbcac35
octalsrc <nicklewchenko92@gmail.com>
2019-02-04 20:50:22 -0700

    Stub out Index2 a bit

--- lib/Octar/Index2.hs
+++ lib/Octar/Index2.hs
@@ -2,8 +2,19 @@
 
 module Octar.Index2 where
 
+import Network.Discard
+import Data.EventGraph
+import Data.EventGraph.Ipfs
 import Data.CARD
-import Octar.Index
+import qualified Octar.Index as Index1
 import Turtle.Ipfs
 
-type Index2 = RGArray IpfsPath
+
+data LiveIndex = LiveIndex
+  { liveConn :: ManagerConn (Edge (IpfsEG String)) String (RGArray IpfsPath)
+  , liveMetaCache :: Index1.MetaCache }
+
+-- | Load entries from the backing store into the metacache, returning
+-- a list of new entries found
+loadIndex :: LiveIndex -> IO (LiveIndex, [IpfsPath])
+loadIndex = undefined
